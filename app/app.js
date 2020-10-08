fff5fe7a469920dd4c0cfb1de3f6d3812485f021
aspecker <specker@gmail.com>
2020-10-08 18:49:31 -0400

    add counts for birthdays by month

--- app/app.js
+++ app/app.js
@@ -1,15 +1,20 @@
 'use strict';
 // Declare app level module which depends on views, and core components
 angular.module('myApp', [
-  'ngRoute',
-  'ngMaterial',
-  'ngMessages',
-  'myApp.view1',
-  'myApp.view2',
-  'myApp.version'
-]).
-config(['$locationProvider', '$routeProvider', function($locationProvider, $routeProvider) {
-  $locationProvider.hashPrefix('!');
+    'ngRoute',
+    'ngMaterial',
+    'ngMessages',
+    'myApp.view1',
+    'myApp.view2',
+    'myApp.version'
+]).config(['$locationProvider', '$routeProvider', '$rootScopeProvider', function ($locationProvider, $routeProvider, $rootScopeProvider) {
+    $locationProvider.hashPrefix('!');
 
-  $routeProvider.otherwise({redirectTo: '/view1'});
-}]);
+    $routeProvider.otherwise({redirectTo: '/view1'});
+}])
+    .run(function ($rootScope) {
+        $rootScope.Utils = {
+            keys: Object.keys,
+            values: Object.values
+        };
+    });
--- app/view1/view1.js
+++ app/view1/view1.js
@@ -2,98 +2,105 @@
 
 angular.module('myApp.view1', ['ngRoute'])
 
-.config(['$routeProvider', function($routeProvider) {
-  $routeProvider.when('/view1', {
-    templateUrl: 'view1/view1.html',
-    controller: 'View1Ctrl'
-  });
-}])
+    .config(['$routeProvider', function ($routeProvider) {
+        $routeProvider.when('/view1', {
+            templateUrl: 'view1/view1.html',
+            controller: 'View1Ctrl'
+        });
+    }])
 
-.factory('apiService', function($http) {
-  var apiURL = 'http://localhost:8080/';
-  function list() {
-    return $http.get(apiURL + 'person');
-  }
+    .factory('apiService', function ($http) {
+        var apiURL = 'http://localhost:8080/';
 
-  function save(data) {
-    return $http({
-      url: apiURL + 'person/save',
-      method: 'POST',
-      headers: {
-        'Content-Type': 'form-data'
-      },
-      data: data
-    });
-  }
+        function list() {
+            return $http.get(apiURL + 'person');
+        }
 
-  return {
-    list: list,
-    save: save
-  };
-})
+        function save(data) {
+            console.log('date of birth inside apiservice' + data.dateOfBirth);
+            return $http({
+                url: apiURL + 'person/save',
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8'
+                },
+                data: jQuery.param(data)
+            });
+        }
 
-.controller('View1Ctrl', ['$scope', 'apiService', '$mdDialog', function($scope, apiService, $mdDialog) {
-  $scope.people = [];
+        function countMonths() {
+            return $http.get(apiURL + 'person/count');
+        }
 
-  // requests person data from API
-  $scope.loadPeople = function() {
-    apiService.list().then(function(response) {
-      $scope.people = response.data;
-    });
-  };
+        return {
+            list: list,
+            save: save,
+            countMonths: countMonths
+        };
+    })
 
+    .controller('View1Ctrl', ['$scope', 'apiService', '$mdDialog', '$rootScope', function ($scope, apiService, $mdDialog, $rootScope) {
+        $scope.people = [];
+        $scope.months = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"];
+        $scope.monthCount = [];
+        // requests person data from API
+        $scope.loadData = function () {
+            apiService.list().then(function (response) {
+                $scope.people = response.data;
+            });
 
-  $scope.showDialog = function (ev) {
-    console.log('showing dialog')
-
-    // show the dialog
-    $mdDialog.show({
-      templateUrl: 'view1/addPersonDialog.tmpl.html',
-      controller: DialogController,
-      targetEvent: ev,
-      clickOutsideToClose: true,
-      parent: angular.element(document.body)
-    }).then(function (didSubmit) {
-      // on dialog submission
-      console.log(didSubmit);
-    }, function(err) {
-      console.log('user closed dialog');
-    });
-  };
-
-  function DialogController($scope, $mdDialog, apiService) {
-    var emptyPersonObject = {
-      firstName: '',
-      lastName: '',
-      dateOfBirth: '',
-      address: ''
-    };
-    $scope.personToAdd = emptyPersonObject;
-    $scope.hide = function () {
-      $mdDialog.hide();
-    };
-
-    $scope.cancel = function () {
-      $scope.personToAdd = emptyPersonObject;
-      $mdDialog.cancel();
-    };
-
-    $scope.submit = function () {
-      console.log($scope.personToAdd);
-      apiService.save($scope.personToAdd)
-          .then(function(result) {
-            console.log(result)
-            // $mdDialog.hide(true);
-          }).catch(function(err) {
-            console.log(err);
-      });
-    };
-  }
-
-}]);
+            apiService.countMonths().then(function (response) {
+                $scope.monthCount = response.data;
+            });
+        };
 
 
+        $scope.showDialog = function (ev) {
 
-// .directive('formatDate', function() {
-  // return "Foo";
-// });
+            // show the dialog
+            $mdDialog.show({
+                templateUrl: 'view1/addPersonDialog.tmpl.html',
+                controller: DialogController,
+                targetEvent: ev,
+                clickOutsideToClose: true,
+                parent: angular.element(document.body)
+            }).then(function (didSubmit) {
+                // on dialog submission
+                // console.log(didSubmit);
+                $scope.loadData();
+            }, function (err) {
+                // console.log('user closed dialog with error' + JSON.stringify(err));
+            });
+        };
+
+        function DialogController($scope, $mdDialog, apiService) {
+            var emptyPersonObject = {
+                firstName: '',
+                lastName: '',
+                dateOfBirth: '',
+                address: ''
+            };
+            $scope.personToAdd = emptyPersonObject;
+            $scope.hide = function () {
+                $mdDialog.hide();
+            };
+
+            $scope.cancel = function () {
+                $scope.personToAdd = emptyPersonObject;
+                $mdDialog.cancel();
+            };
+
+            $scope.submit = function () {
+                $scope.personToAdd.dateOfBirth = new Date($scope.personToAdd.dateOfBirth).toISOString();
+                console.log('passing in date of birth' + $scope.personToAdd.dateOfBirth);
+                apiService.save($scope.personToAdd)
+                    .then(function (result) {
+                        // console.log(result)
+                        $mdDialog.hide(true);
+                    }).catch(function (err) {
+                    // console.log(err);
+                });
+            };
+        }
+
+    }]);
